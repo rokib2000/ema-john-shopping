7fa2d04c4a26c836033121f0d6f8728b896245ac
rokib2000 <rokib0w1@gmail.com>
2022-10-09 15:54:28 +0600

    Septup router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
-import Header from "./components/Header/Header";
+import Orders from "./components/Orders/Orders";
 import Shop from "./components/Shop/Shop";
+import Main from "./layouts/Main";
+import Inventory from './components/Inventory/Inventory'
+import About from './components/About/About'
+
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Shop></Shop>
+        },
+        {
+          path: '/shop',
+          element: <Shop></Shop>
+        },
+        {
+          path: '/orders',
+          element: <Orders></Orders>
+        },
+        {
+          path: 'inventory',
+          element: <Inventory></Inventory>
+        },
+        {
+          path: '/about',
+          element: <About></About>
+        }
+      ]
+    }
+])
+
   return (
     <div>
-      <Header></Header>
-      <Shop></Shop>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
